fa2ce445f0f5d605dda7a4067f3f1ea90ece742f
Max <maxchou0123@outlook.com>
2021-07-11 10:10:03 +0800

    All set for now

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
 module.exports = {
+    plugins: [new MiniCssExtractPlugin()],
+    
     module:{
+       
 
         rules: [
         {
             test: /\.scss$/,
-            use:["style-loader", "css-loader"]
+            use:[MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
         }
-    ]
-    }
+        ]
+    },
+
+    devtool:"source-map"
 }
