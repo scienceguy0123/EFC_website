2b8a54ba559b1df232be2e594dde7fd6f96563e2
Max <maxchou0123@outlook.com>
2021-07-13 18:44:08 +0800

    added fixed and dynamic nav bar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import "./index.scss";
 
-// $(window).load(function() {
-//     $("body").removeClass("preload");
-//   });
+window.addEventListener('scroll', () => {
+    let nav = document.querySelector('.grid-object-1');
+    let windowPosition = window.scrollY > 20;
+    
+    nav.classList.toggle('scroll-nav-active', windowPosition);
+    
+})
